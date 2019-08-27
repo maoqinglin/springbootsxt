7e94355877b9f1b4037ec19d2c79fa53f252ae53
maoqinglin <linmq@snailgame.net>
2019-08-27 11:24:04 +0800

    完成员工编辑功能

--- src/main/java/com/ireadygo/springbootsxt/controller/EmployeeController.java
+++ src/main/java/com/ireadygo/springbootsxt/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -47,6 +49,26 @@ public class EmployeeController {
     @PostMapping("/emp")
     public String addEmp(Employee employee) {
         // 来到员工列表页面
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    // 来到修改页面，查询当前员工，在页面回显
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        // 页面显示所有的部门列表
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("emp", employee);
+        model.addAttribute("depts", departments);
+
+        // 回到修改页面（add是一个修改添加二合一的页面）
+        return "emp/add";
+    }
+
+    // 员工修改，需要提交员工的id
+    @PutMapping("/emp")
+    public String updateEmployee(Employee employee) {
         employeeDao.save(employee);
         return "redirect:/emps";
     }
